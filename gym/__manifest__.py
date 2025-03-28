bbe04f3531ba26b6a866af5c17a568a2bfed6050
josemb21 <josemaria.martinez.eng@outlook.es>
2025-03-28 13:38:57 +0100

    creacion modulo gym

---
+++ gym/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+'name': "gym",
+'summary': """Gestion del modulo Gym""",
+'description': """Gestion de clases, usuarios, material, etc""",
+'author': "ENG - SPA",
+'author': "http://www.upo.es",
+'category': 'Uncategorized',
+'version': '0.1',
+'depends': ['base'],
+'data': [
+'security/security.xml',
+'security/ir.model.access.csv',
+'views/views.xml',
+],
+'demo': [
+'demo/demo.xml',
+],
+'application': True,
+}
