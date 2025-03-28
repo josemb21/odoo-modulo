57a65738829bb3341df3ef4b5c5a4ff9b0e66182
josemb21 <josemaria.martinez.eng@outlook.es>
2025-03-28 12:47:52 +0100

    Update __manifest__.py
    cambio de manifest.py por el de el ejercicio

--- academy/__manifest__.py
+++ academy/__manifest__.py
@@ -1,34 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "academy",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+ 'name': "academy",
+ 'summary': "Academia ENG",
+ 'description': """
+ Academia ENG - Oferta de cursos modulares:
+ - "Python en tres patás"
+ - "Odoo rapidísimo"
+ """,
+ 'author': "Tsianos S.L.",
+ 'website': "http://www.eng.it",
+ 'category': 'Education',
+ 'version': '0.1',
+ 'depends': ['base'],
+ 'data': [
+ # 'security/ir.model.access.csv',
+ 'views/views.xml',
+ 'views/templates.xml',
+ ],
+ 'demo': [
+ 'demo/demo.xml',
+ ],
 }
-
