9b80690884733f8e2b180ecb1efaa7a3a4b52dc7
josemb21 <josemaria.martinez.eng@outlook.es>
2025-03-28 13:58:33 +0100

    Update models.py
    Modificado el modelo gym.gym para convertirlo en gym.session y adaptarlo a la gestión de sesiones de gimnasio, agregando los campos

--- gym/models/models.py
+++ gym/models/models.py
@@ -1,19 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
+class GymSession(models.Model):
+    _name = 'gym.session'
+    _description = 'Modelo para gestionar sesiones de gimnasio'
 
-# class gym(models.Model):
-#     _name = 'gym.gym'
-#     _description = 'gym.gym'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
+    name = fields.Char(string="Nombre de la sesión", required=True)  
+    start_time = fields.Datetime(string="Hora de inicio")
+    end_time = fields.Datetime(string="Hora de fin")
+    instructor_id = fields.Many2one('res.users', string="Instructor")
+    capacity = fields.Integer(string="Capacidad", default=20)
