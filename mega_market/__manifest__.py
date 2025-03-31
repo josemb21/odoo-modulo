ab8d901fac5823abdd5ea1545351b3bb02a4d201
josemb21 <josemaria.martinez.eng@outlook.es>
2025-03-31 14:47:06 +0200

    correcion de errores en los archivos

--- mega_market/__manifest__.py
+++ mega_market/__manifest__.py
@@ -1,34 +1,21 @@
-# -*- coding: utf-8 -*-
 {
     'name': "MegaMarket",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
+    'summary': "Gestión de electrodomésticos y clientes",
     'description': """
-Long description of module's purpose
+        Módulo para gestionar la venta de electrodomésticos y artículos de electrónica.
+        Incluye gestión de clientes y productos.
     """,
-
-    'author': "My Company",
+    'author': "Tu Nombre o Empresa",
     'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'category': 'Sales',
+    'version': '1.0',
     'depends': ['base'],
 
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+      
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'installable': True,
+    'application': True,
 }
-
--- mega_market/models/models.py
+++ mega_market/models/models.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields
 
-class AcademyCourse(models.Model):
-    _name = 'academy.course'
-    _description = 'Academy Course'
+class Electrodomestico(models.Model):
+    _name = 'mega_market.electrodomestico'
+    _description = 'Electrodoméstico'
 
-    name = fields.Char('Course Name', required=True)
-    instructor = fields.Char('Instructor Name', required=True)
-    start_date = fields.Date('Start Date')
-    description = fields.Text('Description')
+    name = fields.Char(string="Nombre", required=True)
+    codigo_producto = fields.Char(string="Código de producto", required=True)
+    pais_id = fields.Many2one('res.country', string='País', required=True)
+    importe_compra = fields.Float(string="Importe de compra")
+    moneda_id = fields.Many2one('res.currency', string='Moneda', required=True)
+    precio_venta = fields.Float(string="Precio de venta")
+
+
+class Cliente(models.Model):
+    _name = 'mega_market.cliente'
+    _description = 'Cliente'
+
+    nombre = fields.Char(string="Nombre", required=True)
+    apellidos = fields.Char(string="Apellidos")
+    nif = fields.Char(string="NIF")
+    direccion = fields.Char(string="Dirección")
+    fecha_nacimiento = fields.Date(string="Fecha de nacimiento")
+    telefono = fields.Char(string="Teléfono")
